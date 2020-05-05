0daaf421c9464a187a12da4b5df90b497139b472
Angel Casadiegos <angelcasadiegos@outlook.es>
2020-05-04 23:42:51 -0500

    Agregar archivos de proyecto.

---
+++ IpsLiquidacionesGUI/ConsultarLiquidacionGUI.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using BLL;
+using ENTITY;
+
+namespace IpsLiquidacionesGUI
+{
+    public partial class ConsultarLiquidacionGUI : Form
+    {
+        LiquidacionModeradoraService liquidacionService= new LiquidacionModeradoraService();
+        RespuestaConsulta respuestaConsulta = new RespuestaConsulta();
+        string TotalLiquidacionesinscritas;
+        string TotalLiquidacionesSubsidiadas;
+        string TotalLiquidacionesContributivas;
+        string TotalCuotasGenerales;
+        string TotalcuotasSubsidiadas;
+        string TotalcuotasContributivas;
+        public ConsultarLiquidacionGUI()
+        {
+            InitializeComponent();
+        }
+
+        private void ConsultarLiquidacionGUI_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CancelarBtn_Click(object sender, EventArgs e)
+        {
+            Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            dataGridView1.DataSource = null;
+            respuestaConsulta = liquidacionService.Consultar();
+            TotalLiquidacionesinscritas = liquidacionService.TotalizarLiquidaciones().ToString();
+            TotalLiquidacionesSubsidiadas = liquidacionService.TotalizarLiquidacionesSubsidiadas().ToString();
+            TotalLiquidacionesContributivas = liquidacionService.TotalizarLiquidacionesContributivas().ToString();
+            TotalCuotasGenerales = liquidacionService.SumarCuotas().ToString();
+
+            Consultar();
+        }
+
+        private void Consultar()
+
+        {
+            string Tipo = TipoConsulta.Text;
+
+            if (Tipo.Equals("Liquidaciones"))
+            {
+                respuestaConsulta = liquidacionService.Consultar();
+                dataGridView1.DataSource = respuestaConsulta.liquidacionesCuotas;
+                TotalCuotasGenerales = liquidacionService.SumarCuotas().ToString();
+                TotalcuotasSubsidiadas = "0";
+                TotalcuotasContributivas = "0";
+            }
+            else if (Tipo.Equals("Subsidiadas"))
+            {
+                dataGridView1.DataSource = liquidacionService.ListaSubsidiadas();
+                TotalLiquidacionesinscritas = TotalLiquidacionesSubsidiadas;
+                TotalLiquidacionesContributivas = "0";
+                TotalCuotasGenerales = "0";
+                TotalcuotasSubsidiadas = liquidacionService.SumarCuotasSubsidiadas().ToString();
+                TotalcuotasContributivas = "0";
+
+            }
+            else if (Tipo.Equals("Contributivas"))
+            {
+                dataGridView1.DataSource = liquidacionService.ListaContributivas();
+                TotalLiquidacionesinscritas = TotalLiquidacionesContributivas;
+                TotalLiquidacionesSubsidiadas = "0";
+                TotalCuotasGenerales = "0";
+                TotalcuotasContributivas = liquidacionService.SumarCuotasContributivas().ToString();
+                TotalcuotasSubsidiadas = "0";
+            }
+            else if (Tipo.Equals("ConsultarXfecha"))
+            {
+
+
+                DateTime fecha = DtConsultarfecha.Value.Date;
+                dataGridView1.DataSource = liquidacionService.ConsultarXFecha(fecha);
+                TotalCuotasGenerales = liquidacionService.ConsultarXFecha(fecha).Sum(l => l.CuotaModeradora).ToString();
+                TotalLiquidacionesinscritas = liquidacionService.ConsultarXFecha(fecha).Count().ToString();
+                TotalLiquidacionesSubsidiadas = "0";
+                TotalLiquidacionesContributivas = "0";
+                TotalcuotasSubsidiadas = "0";
+                TotalcuotasContributivas = "0";
+
+
+            }
+            else if (Tipo.Equals("ConsultarXfechasubsidiadas"))
+            {
+                DateTime fecha = DtConsultarfecha.Value.Date;
+                dataGridView1.DataSource = liquidacionService.ConsultarXFechaSubsidiadas(fecha);
+                TotalLiquidacionesSubsidiadas = liquidacionService.ConsultarXFechaSubsidiadas(fecha).Count().ToString();
+                TotalLiquidacionesinscritas = "0";
+                TotalLiquidacionesContributivas = "0";
+                TotalcuotasSubsidiadas = liquidacionService.ConsultarXFechaSubsidiadas(fecha).Sum(l => l.CuotaModeradora).ToString();
+                TotalCuotasGenerales = "0";
+                TotalcuotasContributivas = "0";
+
+            }
+
+            else if (Tipo.Equals("ConsultarXfechacontributivas"))
+            {
+
+                DateTime fecha = DtConsultarfecha.Value.Date;
+                dataGridView1.DataSource = liquidacionService.ConsultarXFechaContributivas(fecha);
+                TotalLiquidacionesContributivas = liquidacionService.ConsultarXFechaContributivas(fecha).Count().ToString();
+                TotalLiquidacionesinscritas = "0";
+                TotalLiquidacionesSubsidiadas = "0";
+                TotalcuotasContributivas = liquidacionService.ConsultarXFechaContributivas(fecha).Sum(l => l.CuotaModeradora).ToString();
+                TotalCuotasGenerales = "0";
+                TotalcuotasSubsidiadas = "0";
+
+            }
+
+            else if (Tipo.Equals("ConsultaXNombre"))
+            {
+
+
+                dataGridView1.DataSource = liquidacionService.ConsultaPorNombre(ComparaNombretxt.Text);
+                TotalCuotasGenerales = "0";
+                TotalcuotasSubsidiadas = "0";
+                TotalLiquidacionesinscritas = "0";
+                TotalLiquidacionesSubsidiadas = "0";
+                TotalLiquidacionesContributivas = "0";
+                TotalcuotasContributivas = "0";
+
+
+
+
+
+            }
+
+          LlenarCampos();
+        }
+
+        private void LlenarCampos()
+        {
+            Liquidacionestxt.Text =TotalLiquidacionesinscritas;
+           subsidiadastxt.Text = TotalLiquidacionesSubsidiadas;
+             contributivastxt.Text= TotalLiquidacionesContributivas;
+            ValorTotalLiquidacionestxt.Text = TotalCuotasGenerales;
+            TotalSubsidiadastxt.Text= TotalcuotasSubsidiadas;
+            TotalContributivastxt.Text = TotalcuotasContributivas;
+       }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+      
+    }
+}
---
+++ IpsLiquidacionesGUI/PrincipalGUI.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace IpsLiquidacionesGUI
+{
+    partial class PrincipalGUI
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip2 = new System.Windows.Forms.MenuStrip();
+            this.registrarLiquidacionesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.registrarLiquidacionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.consultarLiquidacionToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.menuStrip2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip2
+            // 
+            this.menuStrip2.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registrarLiquidacionesToolStripMenuItem});
+            this.menuStrip2.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip2.Name = "menuStrip2";
+            this.menuStrip2.Size = new System.Drawing.Size(650, 24);
+            this.menuStrip2.TabIndex = 1;
+            this.menuStrip2.Text = "menuStrip2";
+            // 
+            // registrarLiquidacionesToolStripMenuItem
+            // 
+            this.registrarLiquidacionesToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.registrarLiquidacionToolStripMenuItem,
+            this.consultarLiquidacionToolStripMenuItem});
+            this.registrarLiquidacionesToolStripMenuItem.Name = "registrarLiquidacionesToolStripMenuItem";
+            this.registrarLiquidacionesToolStripMenuItem.Size = new System.Drawing.Size(199, 20);
+            this.registrarLiquidacionesToolStripMenuItem.Text = "Menu Liquidaciones Moderadoras";
+            // 
+            // registrarLiquidacionToolStripMenuItem
+            // 
+            this.registrarLiquidacionToolStripMenuItem.Name = "registrarLiquidacionToolStripMenuItem";
+            this.registrarLiquidacionToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.registrarLiquidacionToolStripMenuItem.Text = "Registrar Liquidacion";
+            this.registrarLiquidacionToolStripMenuItem.Click += new System.EventHandler(this.registrarLiquidacionToolStripMenuItem_Click);
+            // 
+            // consultarLiquidacionToolStripMenuItem
+            // 
+            this.consultarLiquidacionToolStripMenuItem.Name = "consultarLiquidacionToolStripMenuItem";
+            this.consultarLiquidacionToolStripMenuItem.Size = new System.Drawing.Size(190, 22);
+            this.consultarLiquidacionToolStripMenuItem.Text = "Consultar Liquidacion";
+            this.consultarLiquidacionToolStripMenuItem.Click += new System.EventHandler(this.consultarLiquidacionToolStripMenuItem_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::IpsLiquidacionesGUI.Properties.Resources.hospital_health_clinic_urban_buildings_medical_icon_1285751;
+            this.pictureBox1.Location = new System.Drawing.Point(98, 128);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(178, 155);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.Desktop;
+            this.panel1.Location = new System.Drawing.Point(378, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(272, 407);
+            this.panel1.TabIndex = 3;
+            // 
+            // PrincipalGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(650, 408);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.menuStrip2);
+            this.Name = "PrincipalGUI";
+            this.Text = "Liquidaciones IPS  SALUDVIDA";
+            this.menuStrip2.ResumeLayout(false);
+            this.menuStrip2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip2;
+        private System.Windows.Forms.ToolStripMenuItem registrarLiquidacionesToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem registrarLiquidacionToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem consultarLiquidacionToolStripMenuItem;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
